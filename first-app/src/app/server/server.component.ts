4fd5c267dbb5c4c49ebfc23bfe5a843ad50c1fcc
sagarkalepu <sagarkalepu99@gmail.com>
2019-01-19 12:49:23 +0530

    added a basic component

---
+++ first-app/src/app/server/server.component.ts
@@ -0,0 +1,8 @@
+import {Component} from "@angular/core";
+import { templateJitUrl } from '@angular/compiler';
+@Component({
+    selector: "srvr",
+    templateUrl: "./server.component.html",
+    styleUrls: []
+})
+export class serverComponent {}
